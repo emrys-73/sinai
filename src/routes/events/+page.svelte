6d4a33e62726e92fe140fdcc986a4410c4cff556
s-jwagn3 <105637911+s-jwagn3@users.noreply.github.com>
2024-09-10 16:54:05 +0200

    added new Event button and landing page for new Events

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,13 +1,22 @@
-<script>
+<script lang="ts">
+	import { goto } from "$app/navigation";
+    import { Button } from "$lib/components/ui/button/index.js";
+  </script>
 
-</script>
 <div class="w-full h-full min-h-screen flex flex-col md:py-10 py-8 md:px-16 px-8 gap-8">
 
     <!-- Header -->
     <div class="w-full md:flex-row flex-col md:gap-6 gap-8 flex justify-between items-center">
         <!-- Title -->
         <div class="flex flex-col w-full">
-            <h1 class="text-white text-4xl font-bold">Event</h1>
+            <h1 class="text-white text-4xl font-bold">Events</h1>
         </div>
+        <div class="w-full h-full flex items-center justify-end">
+            <Button href="/events/new-event" class="bg-black rounded-full font-medium hover:bg-primary transition-all duration-300 ease-in-out hover:text-black">
+                New Event
+            </Button>
+        </div>
+    </div>
+    <div class="w-full md:flex-row flex-col md:gap-6 gap-8 flex items-start justify-start">
     </div>
 </div>
--- src/routes/events/new-event/+page.svelte
+++ src/routes/events/new-event/+page.svelte
@@ -1,13 +1,14 @@
-<script>
+<script lang="ts">
 
-</script>
+ </script>
+
 <div class="w-full h-full min-h-screen flex flex-col md:py-10 py-8 md:px-16 px-8 gap-8">
 
     <!-- Header -->
     <div class="w-full md:flex-row flex-col md:gap-6 gap-8 flex justify-between items-center">
         <!-- Title -->
         <div class="flex flex-col w-full">
-            <h1 class="text-white text-4xl font-bold">Song Pool</h1>
+            <h1 class="text-white text-4xl font-bold">New Event</h1>
         </div>
     </div>
 </div>
