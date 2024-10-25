a689ab48ccdf32ad2c73e02c30bf08ad63507d1f
Adrian Montoya <montoya@astralta.com>
2024-10-25 22:41:14 +0200

    Update Search

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -48,11 +48,11 @@
                 Close chat
               </Button>
             {/if}
-            <Input bind:value={$input} class="rounded-full border-opacity-50 text-white placeholder:text-white placeholder:text-opacity-50" placeholder="Smart-Seaerch coming soon!">
+            <Input bind:value={$input} class="rounded-full border-opacity-50 text-white placeholder:text-white placeholder:text-opacity-50" disabled placeholder="Smart-Search coming soon!">
                 
             </Input>
 
-            <Button type="submit" class="rounded-full text-black">
+            <Button type="submit" class="rounded-full text-black" disabled>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
               </svg>              
